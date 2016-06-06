5e2a3b30f1a5046448ce45e93981098cfbd3dccc
Anton Piatrouski <piatrouski2a@gmail.com>
2016-06-06 16:01:52 +0300

    attribute added

--- src/by/epam/tag/custom/CustomerTag.java
+++ src/by/epam/tag/custom/CustomerTag.java
@@ -1,23 +1,24 @@
 package by.epam.tag.custom;
 
-import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import by.epam.exceptions.DaoException;
 import by.epam.ifaces.ReservationDAO;
 import by.epam.model.beans.Customer;
-import by.epam.model.beans.Payment;
 import by.epam.model.beans.Reservation;
 import by.epam.model.factories.ReservationImplFactory;
 
 @SuppressWarnings("serial")
 public class CustomerTag extends TagSupport {
+	private String var;
+	
+	public void setVar(String var) {
+		this.var = var;
+	}
+	
 	@Override
 	public int doStartTag() throws JspException {
 		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
@@ -27,33 +28,10 @@ public class CustomerTag extends TagSupport {
 			ReservationDAO reservDao = ReservationImplFactory.getImplementation();
 			Reservation reserv = reservDao.getReservation(fileName);
 			Customer customer = reserv.getCustomer();
-			List<Payment> payments = customer.getPayments();
 			
-			String firstName = "<tr><td>FirstName</td><td>" + customer.getFirstName() + "</td></tr>";
-			String lastName = "<tr><td>LastName</td><td>" + customer.getLastName() + "</td></tr>";
-			String emailAddress = "<tr><td>EmailAddress</td><td>" + customer.getEmailAddress() + "</td></tr>";
-			String phoneNumber = "<tr><td>PhoneNumber</td><td>" + customer.getPhoneNumber() + "</td></tr>";
+			pageContext.setAttribute(var, customer);
 			
-			StringBuilder paymentsSb = new StringBuilder();
-			
-			for (Payment payment : payments) {
-				String amountPaid = "<tr><td>AmountPaid</td><td>" + payment.getAmountPaid() + "</td></tr>";
-				String formOfPaymentTypeCode = "<tr><td>FormOfPaymentTypeCode</td><td>" + payment.getFormOfPaymentTypeCode() + "</td></tr>";
-				String currencyCode = "<tr><td>CurrencyCode</td><td>" + payment.getCurrencyCode() + "</td></tr>";
-				paymentsSb.append(amountPaid);
-				paymentsSb.append(formOfPaymentTypeCode);
-				paymentsSb.append(currencyCode);
-			}
-			
-			JspWriter out = pageContext.getOut();
-			out.write("<table class=\"table table-striped\">");
-			out.write(firstName);
-			out.write(lastName);
-			out.write(emailAddress);
-			out.write(phoneNumber);
-			out.write(paymentsSb.toString());
-			out.write("</table>");
-		} catch(IOException | DaoException e) {
+		} catch(DaoException e) {
 			throw new JspException(e.getMessage());
 		}
 		return SKIP_BODY;
--- src/by/epam/tag/custom/FareFamilyTag.java
+++ src/by/epam/tag/custom/FareFamilyTag.java
@@ -1,23 +1,24 @@
 package by.epam.tag.custom;
 
-import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 import by.epam.exceptions.DaoException;
 import by.epam.ifaces.ReservationDAO;
-import by.epam.model.beans.AncillaryAirComponent;
 import by.epam.model.beans.FareFamily;
 import by.epam.model.beans.Reservation;
 import by.epam.model.factories.ReservationImplFactory;
 
 @SuppressWarnings("serial")
 public class FareFamilyTag extends TagSupport {
+	private String var;
+	
+	public void setVar(String var) {
+		this.var = var;
+	}
+	
 	@Override
 	public int doStartTag() throws JspException {
 		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
@@ -27,23 +28,10 @@ public class FareFamilyTag extends TagSupport {
 			ReservationDAO reservDao = ReservationImplFactory.getImplementation();
 			Reservation reserv = reservDao.getReservation(fileName);
 			FareFamily fareFamily = reserv.getFareFamily();
-			List<AncillaryAirComponent> components = fareFamily.getAncillaryAirComponents();
 			
-			String fareFamilyCode = "<tr><td>FareFamilyCode</td><td>" + fareFamily.getFareFamilyCode() + "</td></tr>";
+			pageContext.setAttribute(var, fareFamily);
 			
-			StringBuilder componentsSb = new StringBuilder();
-			
-			for (AncillaryAirComponent component : components) {
-				String ancillaryAirComponentCode = "<tr><td>AncillaryAirComponentCode</td><td>" + component.getAncillaryAirComponentCode() + "</td></tr>";
-				componentsSb.append(ancillaryAirComponentCode);
-			}
-			
-			JspWriter out = pageContext.getOut();
-			out.write("<table class=\"table table-striped\">");
-			out.write(fareFamilyCode);
-			out.write(componentsSb.toString());
-			out.write("</table>");
-		} catch(IOException | DaoException e) {
+		} catch(DaoException e) {
 			throw new JspException(e.getMessage());
 		}
 		return SKIP_BODY;
